a481b86ff2388af3e503fdc7fbc6e7abcd0c82e7
wellington <wellington.frandin@c-137labs.com>
2025-12-06 17:05:04 -0300

    fix url prod

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -11,10 +11,8 @@ import type {
   UploadResponse,
 } from "@/types";
 
-const API_URL = "http://localhost:3000";
-
 export const api = axios.create({
-  baseURL: API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
